6a6ce6c42dc8a6aaf765658a6ad9449e5d112dc8
Azodox <likemoi99@gmail.com>
2024-01-23 09:05:01 +0100

    feat: support for mysql (base)

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: process.env.DATABASE_HOST,
+    port: +process.env.DATABASE_PORT,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    autoLoadEntities: true
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
