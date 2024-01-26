d280f7e89990ef7441931e8316ce1dcff7696ce6
Azodox <likemoi99@gmail.com>
2024-01-26 21:06:06 +0100

    fix: removed redundant debug

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,7 +4,6 @@ import 'dotenv/config'
 import * as process from "process";
 
 async function bootstrap() {
-  console.log(process.env.DATABASE_HOST)
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
