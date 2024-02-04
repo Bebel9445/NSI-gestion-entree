886b41f6b1f4661a5735a66da94a0b84f39131b0
Azodox <likemoi99@gmail.com>
2024-02-04 12:24:17 +0100

    fix: added utf8 encoding to https certificate reading

--- api/src/main.ts
+++ api/src/main.ts
@@ -8,19 +8,18 @@ async function bootstrap() {
   const isHttps: boolean = process.env.HTTPS === 'true'
   const httpsOptions = isHttps
     ? {
-        key: readFileSync(process.env.HTTPS_PRIVATE_KEY_PATH),
-        cert: readFileSync(process.env.HTTPS_PUBLIC_CERTIFICATE_PATH),
+        key: readFileSync(process.env.HTTPS_PRIVATE_KEY_PATH, 'utf-8'),
+        cert: readFileSync(process.env.HTTPS_PUBLIC_CERTIFICATE_PATH, 'utf-8'),
       }
     : {}
-  
+
   let app: NestApplication
   if (isHttps) {
     app = await NestFactory.create(AppModule, {
       httpsOptions,
     })
-  } else
-    app = await NestFactory.create(AppModule)
-  
+  } else app = await NestFactory.create(AppModule)
+
   await app.listen(3000)
 }
 bootstrap()
