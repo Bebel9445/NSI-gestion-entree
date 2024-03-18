da6e04b66025395c1635d7db7922b50b7ace7412
Azodox <likemoi99@gmail.com>
2024-03-18 22:07:23 +0100

    fix: missing ApikeyModule import in UsersModule

--- api/src/users/users.module.ts
+++ api/src/users/users.module.ts
@@ -3,10 +3,11 @@ import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UsersController } from './users.controller';
+import { ApikeyModule } from 'src/auth/apikey/apikey.module'
 
 @Module({
   providers: [UsersService],
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [ApikeyModule, TypeOrmModule.forFeature([User])],
   exports: [UsersService],
   controllers: [UsersController]
 })
