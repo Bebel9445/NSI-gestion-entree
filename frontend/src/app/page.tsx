6b9c472b0051cb335ee874a18df8cf9b6928d4c5
BARLIER Luke <lbarlier@JBS.LOCAL>
2024-04-08 15:21:36 +0200

    fix: updated '/' page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,60 +1,50 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "../../node_modules/next/link";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.topnav}>
+      <nav className={styles.topnav}>
         <Image
-        src="/Logo v4 (Tigre).jpg"
-        width={100}
-        height={50}
-        className={styles.logo}
+          src="/Logo v4 (Tigre).jpg"
+          width={100}
+          height={50}
+          className={styles.logo}
         />
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <li>
-            Identification
-          </li>
-        </a>
+        <li>
+          Recharger
+        </li>
+        <li>
+          Connexion
+        </li>
+        <li>
+          <Link href="/register">Inscription</Link>
+        </li>
+      </nav>
+      
+      <section className={styles.section}>
+      </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <li>
-            Qui sommes-nous ? 
-          </li>
-        </a>
+      <section className={styles.section}>
+        <hr style={{color: "black", width: "100vw"}}/>
+      </section>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <li>
-            Rechargez 
-          </li>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <li>
-            Crédits </li>
-        </a>
-      </div>
-    <p>Issoupe Inc.©</p>
+      <footer className={styles.footer}>
+        <div className={styles.one}>
+          <div className={styles.vl}>
+            <ul className={styles.footerList}>
+              <li><a href="mailto:christinafit@orange.fr">Contact</a></li>
+            </ul>  
+          </div>
+        </div>
+        <div className={styles.two}>
+          <div className={styles.vl}></div>
+        </div>
+        <div className={styles.copyright}>
+          <p>Issoupe Inc.©</p>
+        </div>
+      </footer>
     </main>
   );
 }
