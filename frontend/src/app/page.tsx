9cd2c14fe5265a8ca10b24ee89dd7ec8e5c3baae
Gagoune <116149993+Gagoune@users.noreply.github.com>
2024-04-08 14:58:19 +0200

    Nouvelle page (refill)
    nouvelle page refill

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,26 +1,22 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.topnav}>
         <Image
-        src="/Logo v4 (Tigre).jpg"
-        width={100}
-        height={50}
-        className={styles.logo}
-        />
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+          src="/Logo v4 (Tigre).jpg"
+          width={100}
+          height={50}
+          className={styles.logo} alt={""}        />
+        <Link href="http://localhost:3000/register"
         >
           <li>
             Identification
           </li>
-        </a>
+        </Link>
 
         <a
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -34,8 +30,7 @@ export default function Home() {
         </a>
 
         <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
+          href="http://localhost:3000/refill"
           target="_blank"
           rel="noopener noreferrer"
         >
